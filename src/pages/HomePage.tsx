1532240596e2a7a97ea75d5ca5b37a50bbbe5df5
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2026-02-21 04:45:23 +0900

    feat: Header 컴포넌트

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Header from "../components/Header";
+
 const HomePage = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <h1>HomePage</h1>
-        </div>
+        <Wrapper>
+            <Container>
+                <Header />
+            </Container>
+        </Wrapper>
     );
 };
 
 export default HomePage;
+
+const Wrapper = styled.div`
+    background: #FFDE21;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    box-sizing: border-box;
+`;
